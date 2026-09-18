43df91facbe7df8442447203e9564158bfef272b
joshua ofarrell <joshuaofarrell88@gmail.com>
2026-09-18 08:39:47 -0500

    First update with Getters

--- Product2.java
+++ Product2.java
@@ -1,22 +1,34 @@
-public public class Product2 {
+public class Product2 {
     protected int mProductId;
     protected int mManufactureId;
     protected float mWholeSalePrice;
     protected float mMarkup;
 
-    public Product(){
+    public Product2(){
         mProductId = 12345;
         mManufactureId = 0123;
         mWholeSalePrice = 25.99f;
-        mMarkup = 0.05f;
+        mMarkup = .30f;
     }
-    public Product(int productId, int manufactureId, float wholeSalePrice, float markup){
+    public Product2(int productId, int manufactureId, float wholeSalePrice, float markup){
         mProductId = productId;
         mManufactureId = manufactureId;
         mWholeSalePrice = wholeSalePrice;
         mMarkup = markup;
     }
-}
- Main {
+    public int getProductId() {
+        return mProductId;
+    }
+    public int getManufactureId() {
+        return mManufactureId;
+    }
+    public float getWholeSalePrice() {
+        return mWholeSalePrice;
+    }
+    public float getMarkup() {
+        return mMarkup;
+    }
     
+
 }
+    
